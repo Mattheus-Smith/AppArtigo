1c9a195aab257d7fd97a10643e1a7c250710b1ba
moon light <mscf@icomp.ufam.edu.br>
2023-02-17 21:42:08 -0400

    att funcao da transformada de fourier - parte 2

--- python/funcoes/2transformadaFourier_por_canal.py
+++ python/funcoes/2transformadaFourier_por_canal.py
@@ -7,25 +7,84 @@ import numpy as np
 from matplotlib import pyplot as plt
 from pylab import *
 
-img = cv2.imread("./../1imagensEntrada/10.jpg")
-img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)    # Converte para escala de cinza
+def caso1_por_canal():
+    img = cv2.imread("./../1imagensEntrada/10.jpg")
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  # Converte para escala de cinza
 
-B, G, R = cv2.split(img)
+    B, G, R = cv2.split(img)
 
-# # https://hicraigchen.medium.com/digital-image-processing-using-fourier-transform-in-python-bcb49424fd82
-plt.figure(figsize=(10,5), constrained_layout=False)
-plt.subplot(321), plt.imshow(R, "gray"), plt.title("Original Image")
+    # # https://hicraigchen.medium.com/digital-image-processing-using-fourier-transform-in-python-bcb49424fd82
+    plt.figure(figsize=(10, 5), constrained_layout=False)
+    plt.subplot(321), plt.imshow(R, "gray"), plt.title("Original Image")
 
-original = np.fft.fft2(R)
-plt.subplot(322), plt.imshow(np.log(1+np.abs(original)), "gray"), plt.title("Spectrum")
+    img_transformed = np.fft.fft2(R)
+    plt.subplot(322), plt.imshow(np.log(1 + np.abs(img_transformed)), "gray"), plt.title("Spectrum")
 
-center = np.fft.fftshift(original)
-plt.subplot(323), plt.imshow(np.log(1+np.abs(center)), "gray"), plt.title("Centered Spectrum")
+    img_transformed_shift = np.fft.fftshift(img_transformed)
+    plt.subplot(323), plt.imshow(np.log(1 + np.abs(img_transformed_shift)), "gray"), plt.title("Centered Spectrum")
 
-inv_center = np.fft.ifftshift(center)
-plt.subplot(324), plt.imshow(np.log(1+np.abs(inv_center)), "gray"), plt.title("Decentralized")
+    # H
+    # filter: low passa filter
+    M, N = img_transformed.shape
+    H = np.zeros((M, N), dtype=np.float32)
+    D0 = 50
+    for u in range(M):
+        for v in range(N):
+            D = np.sqrt((u - M / 2) ** 2 + (v - M / 2) ** 2)
+            if (D <= D0):
+                H[u, v] = 1
+            else:
+                H[u, v] = 0
 
-processed_img = np.fft.ifft2(inv_center)
-plt.subplot(325), plt.imshow(np.abs(processed_img), "gray"), plt.title("Processed Image")
+    # g = img_transformed_shift * H
+    Gshiht = img_transformed_shift * H
 
-plt.show()
+    inv_transformed = np.fft.ifftshift(Gshiht)
+    # inv_transformed= np.fft.ifftshift(img_transformed_shift)
+    plt.subplot(324), plt.imshow(np.log(1 + np.abs(inv_transformed)), "gray"), plt.title("Decentralized")
+
+    processed_img = np.fft.ifft2(inv_transformed)
+    plt.subplot(325), plt.imshow(np.abs(processed_img), "gray"), plt.title("Processed Image")
+
+    plt.show()
+
+def novo_caso():
+    #https://www.youtube.com/watch?v=YVBxM64kpkU&ab_channel=MadePython
+    img = cv2.imread("./../1imagensEntrada/10.jpg")
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)    # Converte para escala de cinza
+
+    plt.figure(figsize=(10,5), constrained_layout=False)
+    plt.subplot(321), plt.imshow(img_gray, "gray"), plt.title("Original Image")
+
+    img_transformed = np.fft.fft2(img_gray)
+    plt.subplot(322), plt.imshow(np.log(1+np.abs(img_transformed)), "gray"), plt.title("Spectrum")
+
+    img_transformed_shift = np.fft.fftshift(img_transformed)
+    plt.subplot(323), plt.imshow(np.log(1+np.abs(img_transformed_shift)), "gray"), plt.title("Centered Spectrum")
+
+    #H
+    # filter: low passa filter
+    M, N = img_gray.shape
+    H = np.zeros((M,N), dtype=np.float32)
+    D0 = 50
+    for u in range(M):
+        for v in range(N):
+            D = np.sqrt((u-M/2)**2 + (v-N/2)**2)
+            if( D <= D0):
+                H[u,v] = 1
+            else:
+                H[u, v] = 0
+
+
+    # g = img_transformed_shift * H
+    Gshiht = img_transformed_shift * H
+    plt.subplot(324), plt.imshow(np.log(1+np.abs(Gshiht)), "gray"), plt.title("Decentralized")
+
+    inv_transformed= np.fft.ifftshift(Gshiht)
+    plt.subplot(325), plt.imshow(np.abs(inv_transformed), "gray"), plt.title("Processed Image")
+
+    output = np.abs(np.fft.ifft(inv_transformed))
+    plt.subplot(325), plt.imshow(np.abs(output), "gray"), plt.title("Processed Image")
+    plt.show()
+
+novo_caso()
